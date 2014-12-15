fb33c13c95da3c7942684127793f0299ad07699e
Jacob Rieger <jacob@talentriot.com>
2014-12-15 10:57:29 -0700

    Added colorbox fix for edges being cutoff

--- oneteam/js/oneteam.js
+++ oneteam/js/oneteam.js
@@ -3,25 +3,25 @@ $(document).ready(function () {
 
         $(document).on('click', '.video-overlay', function() {
             var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-2299698\" mozallowfullscreen name=\"vzvd-2299698\" src=\"http://view.vzaar.com/2299698/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
-            $.colorbox({ html: iframe, height: 345, width: 565 });
+            $.colorbox({ html: iframe});
         });
 
         $(document).on('click', '.video-overlay-small-1', function () {
             //Open modal with video content inside
             var iframe = " <iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1462998\" mozallowfullscreen name=\"vzvd-1462998\" src=\"http://view.vzaar.com/1462998/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
-            $.colorbox({ html: iframe, height: 345, width: 565 });
+            $.colorbox({ html: iframe});
         });
 
         $(document).on('click', '.video-overlay-small-2', function () {
             //Open modal with video content inside
             var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1551614\" mozallowfullscreen name=\"vzvd-1551614\" src=\"http://view.vzaar.com/1551614/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
-            $.colorbox({ html: iframe, height: 345, width: 565 });
+            $.colorbox({ html: iframe});
         });
 
         $(document).on('click', '.video-overlay-small-3', function () {
             //Open modal with video content inside
             var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1551668\" mozallowfullscreen name=\"vzvd-1551668\" src=\"http://view.vzaar.com/1551668/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
-            $.colorbox({ html: iframe, height: 345, width: 565 });
+            $.colorbox({ html: iframe});
         });
 
 
