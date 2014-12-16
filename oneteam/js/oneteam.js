0d574f0e3be883c6f9b27c6d8f390bdcea988673
Jacob Rieger <jacob@talentriot.com>
2014-12-16 12:28:43 -0700

    Latest staging changes.
    Video width on small screens
    Internet explorer fixes
    Buy link updates

--- oneteam/js/oneteam.js
+++ oneteam/js/oneteam.js
@@ -2,26 +2,61 @@ $(document).ready(function () {
     $("#footer").load("./footer.html"); 
 
         $(document).on('click', '.video-overlay', function() {
-            var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-2299698\" mozallowfullscreen name=\"vzvd-2299698\" src=\"http://view.vzaar.com/2299698/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
-            $.colorbox({ html: iframe, fixed:true, top:"20%"});
+            if($(window).width() <= 593)
+            {
+                var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-2299698\" mozallowfullscreen name=\"vzvd-2299698\" src=\"http://view.vzaar.com/2299698/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"250\" height=\"139\"></iframe>";
+                $.colorbox({ html: iframe, fixed:true, top:"5%"});
+            }
+            else
+            {
+                var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-2299698\" mozallowfullscreen name=\"vzvd-2299698\" src=\"http://view.vzaar.com/2299698/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
+                $.colorbox({ html: iframe, fixed:true, top:"20%"});
+            }
         });
 
         $(document).on('click', '.video-overlay-small-1', function () {
             //Open modal with video content inside
-            var iframe = " <iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1462998\" mozallowfullscreen name=\"vzvd-1462998\" src=\"http://view.vzaar.com/1462998/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
-            $.colorbox({ html: iframe, fixed:true, top:"20%"});
+            
+            if($(window).width() <= 593)
+            {
+                var iframe = " <iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1462998\" mozallowfullscreen name=\"vzvd-1462998\" src=\"http://view.vzaar.com/1462998/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"250\" height=\"139\"></iframe>";
+                $.colorbox({ html: iframe, fixed:true, top:"5%"});
+            }
+            else
+            {
+                var iframe = " <iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1462998\" mozallowfullscreen name=\"vzvd-1462998\" src=\"http://view.vzaar.com/1462998/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
+                $.colorbox({ html: iframe, fixed:true, top:"20%"});
+            }
         });
 
         $(document).on('click', '.video-overlay-small-2', function () {
             //Open modal with video content inside
-            var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1551614\" mozallowfullscreen name=\"vzvd-1551614\" src=\"http://view.vzaar.com/1551614/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
-            $.colorbox({ html: iframe, fixed:true, top:"20%"});
+            
+            if($(window).width() <= 593)
+            {
+                var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1551614\" mozallowfullscreen name=\"vzvd-1551614\" src=\"http://view.vzaar.com/1551614/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"250\" height=\"211\"></iframe>";
+                $.colorbox({ html: iframe, fixed:true, top:"5%"});
+            }
+            else
+            {
+                var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1551614\" mozallowfullscreen name=\"vzvd-1551614\" src=\"http://view.vzaar.com/1551614/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
+                $.colorbox({ html: iframe, fixed:true, top:"20%"});
+            }
         });
 
         $(document).on('click', '.video-overlay-small-3', function () {
             //Open modal with video content inside
-            var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1551668\" mozallowfullscreen name=\"vzvd-1551668\" src=\"http://view.vzaar.com/1551668/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
-            $.colorbox({ html: iframe, fixed:true, top:"20%"});
+            
+            if($(window).width() <= 593)
+            {
+                var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1551668\" mozallowfullscreen name=\"vzvd-1551668\" src=\"http://view.vzaar.com/1551668/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"250\" height=\"139\"></iframe>";
+                $.colorbox({ html: iframe, fixed:true, top:"5%"});
+            }
+            else
+            {
+                var iframe = "<iframe allowfullscreen allowtransparency=\"true\" class=\"vzaar-video-player\" frameborder=\"0\" id=\"vzvd-1551668\" mozallowfullscreen name=\"vzvd-1551668\" src=\"http://view.vzaar.com/1551668/player\" title=\"vzaar video player\" type=\"text/html\" webkitallowfullscreen width=\"495\" height=\"275\"></iframe>";
+                $.colorbox({ html: iframe, fixed:true, top:"20%"});
+            }
         });
 
 
